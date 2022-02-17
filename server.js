064ad2dec1d20b49a81daa8d107cba4a1ee9f147
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-02-16 18:29:46 -0600

    portTryOne

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const api = require("./api/notes");
 const app = express();
 const path = require('path')
 
+const PORT = process.env.PORT || 3000
+
 // const id = require("./api/notes/id")
 
 // built-in express middleware function serving CSS files
@@ -17,37 +19,5 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
 
-// app.get('/:id', (res, req) => {
-//     res.sendFile(path.join(__dirname, 'public/notes.html'))
-// })
+app.listen(PORT);
 
-
-
-app.listen(3000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // you'll either be sending down some JSON or rendering a file with Express.js (mainly)
-    // RENDER A FILE
-    // res.render('index')
-
-    // DOWNLOADABLE FILE send a file to user to download and pass the path
-    // res.download('public/assets/js/server.js')
-
-    // consoles {"message":"Error"}
-    // res.json({ message: "Error" })
-    
-    // above the app api /api 
-    // app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'index.html'))
-// })
